8c620f2589d06475036077c1d783f734de4a89b4
rtemb <darakon92@gmail.com>
2017-07-05 18:28:49 +0300

    Add logger. Remove some imports

--- handlers.go
+++ handlers.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strconv"
-	"time"
 
 	"github.com/takama/router"
 )
@@ -16,4 +9,13 @@ import (
 // root endpoint
 func root(c *router.Control) {
 	fmt.Fprintf(c.Writer, "Processing URL %s...", c.Request.URL.Path)
+}
+
+// logger provides a log of requests
+func logger(c *router.Control) {
+	remoteAddr := c.Request.Header.Get("X-Forwarded-For")
+	if remoteAddr == "" {
+		remoteAddr = c.Request.RemoteAddr
+	}
+	log.Infof("%s %s %s", remoteAddr, c.Request.Method, c.Request.URL.Path)
 }
